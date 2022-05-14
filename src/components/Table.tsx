25f415176ed90f09dab0eb49cf7cc75108cb8e6d
Jibin Babu <jibinbabu966@gmail.com>
2022-05-14 08:19:14 +0530

    debouncing

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,12 +1,23 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+
+import { ChangeEvent, useEffect, useState } from "react";
+
+import "./Table.css";
+
+import { useDebounce } from "usehooks-ts";
+
 import InfiniteScroll from "react-infinite-scroll-component";
+
 export const Table = () => {
   const [items, setItems] = useState([]);
 
   const [noMore, setNoMore] = useState(true);
 
   const [page, setPage] = useState(2);
+
+  const [filterData, setFilterData] = useState([]);
+
+  const [value, setValue] = useState<string>("");
 
   useEffect(() => {
     const getStudents = async () => {
@@ -33,56 +44,79 @@ export const Table = () => {
         setPage(page + 1);
       });
   };
-  console.log(items);
+
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  const handleFilter = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    if (debouncedValue.length >= 3) {
+      axios
+        .get(`http://localhost:5050/students?q=${debouncedValue}`)
+        .then((res) => {
+          setFilterData(res.data);
+        });
+    } else {
+      setFilterData([]);
+    }
+  }, [debouncedValue]);
+
   return (
-    <div>
-      <input placeholder="Seacrh name..." type="text" />
-      <div>
-        {" "}
-        {items.map((val) => {
-          return <div>{val.name}</div>;
-        })}{" "}
+    <div className="container">
+      <input placeholder="Seacrh name..." type="text" onChange={handleFilter} />
+      {filterData.length !== 0 && (
+        <div className="data-list">
+          {" "}
+          {filterData.map((val) => {
+            return <div className="data-item">{val.name}</div>;
+          })}{" "}
+        </div>
+      )}
+
+      <div className="scroll-container">
+        <InfiniteScroll
+          dataLength={items.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={noMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <table className="table table-success table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Image</th>
+                <th scope="col">LecturesAttended</th>
+                <th scope="col">TotalLectures</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items &&
+                items.map((item) => {
+                  return (
+                    <tr>
+                      <th scope="row">{item.id}</th>
+                      <td>{item.name}</td>
+                      <td>
+                        {" "}
+                        <img src={item.avatarURL} alt="Img" />{" "}
+                      </td>
+                      <td>{item.lecturesAttended}</td>
+                      <td>{item.totalLectures}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </InfiniteScroll>
       </div>
-      <InfiniteScroll
-        dataLength={items.length} //This is important field to render the next data
-        next={fetchData}
-        hasMore={noMore}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        <table className="table table-success table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Image</th>
-              <th scope="col">LecturesAttended</th>
-              <th scope="col">TotalLectures</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items &&
-              items.map((item) => {
-                return (
-                  <tr>
-                    <th scope="row">{item.id}</th>
-                    <td>{item.name}</td>
-                    <td>
-                      {" "}
-                      <img src={item.avatarURL} alt="Img" />{" "}
-                    </td>
-                    <td>{item.lecturesAttended}</td>
-                    <td>{item.totalLectures}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </InfiniteScroll>
     </div>
   );
 };
